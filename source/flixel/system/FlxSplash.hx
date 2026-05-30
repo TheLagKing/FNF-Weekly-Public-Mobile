c6e64e846eb3ba68e61a5f313b9ccd068e03e4e4
TheLagKing <dalucanogheaven12@gmail.com>
2026-05-31 03:05:21 +0800

    Update FlxSplash.hx

--- source/flixel/system/FlxSplash.hx
+++ source/flixel/system/FlxSplash.hx
@@ -14,6 +14,8 @@ import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
 
+import gameObjects.Video4;
+
 import meta.states.*;
 import meta.data.*;
 
@@ -38,7 +40,7 @@ class FlxSplash extends MusicBeatState
 	var _cachedTimestep:Bool;
 	var _cachedAutoPause:Bool;
 
-	var video:FlxVideo;
+	var video:Video4;
 
 	override public function create():Void
 	{
@@ -57,12 +59,19 @@ class FlxSplash extends MusicBeatState
 		#end
 
 		new FlxTimer().start(1, function(tmr:FlxTimer){
-			video = new FlxVideo();
-			video.load(Paths.video('intro'));
-			video.onEndReached.add(function() {
-				onComplete();
+			video = new Video4();
+			video.isStateAffected = false;
+
+			video.onFormat(()->{
+				video.setGraphicSize(FlxG.width, FlxG.height);
+				video.updateHitbox();
 			});
-			video.play();
+				
+			if (video.load(Paths.video('intro')))
+			{
+				video.delayAndStart();
+			}
+			add(video);
 		});
 
 		// _times = [0.041, 0.184, 0.334, 0.495, 0.636];
@@ -101,22 +110,6 @@ class FlxSplash extends MusicBeatState
 	}
 
 	override function update(elapsed:Float) {
-		var justTouched:Bool = false;
-
-		#if mobile
-                for (touch in FlxG.touches.list)
-	                if (touch.justPressed)
-		                justTouched = true;
-		#end
-		
-		if (FlxG.keys.justPressed.SPACE || FlxG.keys.justPressed.ENTER || justTouched) {
-			if (video != null) {
-				video.stop();
-				onComplete();
-			}
-
-
-		}
 		super.update(elapsed);
 	}
 

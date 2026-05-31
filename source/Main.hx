b6b836093f7c5bf0185ccfb52c57314055e49f5e
TheLagKing <dalucanogheaven12@gmail.com>
2026-05-31 23:51:27 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -20,12 +20,13 @@ import meta.CompilationStuff;
 
 import lime.app.Application;
 import lime.system.System as LimeSystem;
+import flixel.system.FlxSplash;
 
 class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var initialState:Class<FlxState> = Init; // The FlxState the game starts with.
+	var initialState:Class<FlxState> = Flxsplash; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
@@ -45,6 +46,7 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
+        mobile.backend.CrashHandler.init();
 
 		if (stage != null)
 		{
@@ -104,6 +106,8 @@ class Main extends Sprite
 		// initialState = TitleState;		
 		// #end
 		// #end
+
+        FlxSplash.nextState = Init;
 
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FNFGame(gameWidth, gameHeight, initialState, #if (flixel < "5.0.0") zoom, #end framerate, framerate, skipSplash, startFullscreen));
